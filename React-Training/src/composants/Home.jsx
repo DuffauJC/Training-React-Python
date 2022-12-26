1d5d879c636bd40a8282365a587f287807a8c270
Jean-Charles <jeancharles.duffau@gmail.com>
2022-12-26 21:24:56 +0100

    premier composant home

---
+++ React-Training/src/composants/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+
+
+
+  return (
+      <div class="container home">
+          <h1>Welcome on Trainings Modern Shop</h1>
+      </div>
+  )
+}
